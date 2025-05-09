5d5c35bd094cb909f0e91f95e55e42ff192d3dbf
Shahid Qureshi <qureshi.shahid7777@gmail.com>
2025-05-09 16:25:55 +0530

    removed base configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // Get the base path depending on environment
-const isProduction = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: isProduction ? "/money-mind-client" : "/",
 });
